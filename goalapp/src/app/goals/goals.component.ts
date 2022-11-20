43ecec337f68d318d4b77755e6def3f065d97414
Manoj Kumar <manoj.cs08@gmail.com>
2022-11-20 17:40:24 +0530

    progress bar

--- goalapp/src/app/goals/goals.component.ts
+++ goalapp/src/app/goals/goals.component.ts
@@ -11,16 +11,14 @@ import { GoalsService } from './goals.service';
       <header class="card-header">
         <p class="card-header-title">My Goal Lists</p>
       </header>
-      <div class="card-content" *ngFor="let goal of goaldata">
+      <div class="card-content" *ngFor="let goal of finalGoalData">
         <div class="content">
           <p><b>Goal Name: </b>{{goal.title}}</p>
           <p><b>Description: </b> {{goal.description}}</p>
           <p> <b>Dead-line: </b> {{goal.deadline | date: 'dd/MM/yyyy' }}</p>
           <p>Status: in-progress</p>
-          <strong>Progress: </strong>
-          <progress class="progress is-success" value="20" max="100">
-            33%
-          </progress>
+          <strong>Progress: {{goal.progress}}%</strong>
+          <progress class="progress is-success" value="{{goal.progress}}" max="100">{{goal.progress}}%</progress>
         </div>       
         <a (click)="steps(goal._id)" class="button is-success m-2">
           Steps</a>
@@ -37,7 +35,41 @@ export class GoalsComponent implements OnInit {
   userid = localStorage.getItem("userid");
   goalsList:any;
   goaldata:any;
-  constructor(private router: Router,private goalService: GoalsService) {}
+  finalGoalData:any=[];
+  constructor(private router: Router,private goalService: GoalsService) {
+    this.goalService.getGoals().subscribe((response) => {
+      this.goalsList = response;
+      this.goaldata = this.goalsList.data;
+      //console.log(this.goaldata.length);
+      if(this.goaldata.length){
+        
+        for(let i=0;i<this.goaldata.length;i++){
+          let tempArr: any=[];
+          let completed_steps = 0;
+          let progress = 0;
+          tempArr['_id'] = this.goaldata[i]._id;
+          tempArr['title'] = this.goaldata[i].title;
+          tempArr['description'] = this.goaldata[i].description;
+          tempArr['deadline'] = this.goaldata[i].deadline;
+          let total_steps = this.goaldata[i].steps.length;
+          if(this.goaldata[i].steps.length){            
+            for(let j=0;j<this.goaldata[i].steps.length;j++){
+              //console.log(this.goaldata[i].steps[j]);
+                if(this.goaldata[i].steps[j].status =='completed'){
+                  completed_steps++;
+                }
+            }
+          }
+          if(total_steps>0 && completed_steps>0){
+            progress = ((completed_steps/total_steps)*100);
+          }
+          tempArr['progress'] = progress;
+          this.finalGoalData.push(tempArr);
+        }
+      }
+      //console.log(this.finalGoalData);
+    });
+  }
   add() {
     this.router.navigate(['goals', 'add']);
   }
@@ -54,9 +86,9 @@ export class GoalsComponent implements OnInit {
   }
 
   ngOnInit(): void {
-    this.goalService.getGoals().subscribe((response) => {
-      this.goalsList = response;
-      this.goaldata = this.goalsList.data;
-    });
+   
+    
   }
+
+ 
 }
